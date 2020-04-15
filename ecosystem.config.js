735a1db9d202a749f02aeb52219b918c12d5f390
Agent1998 <msurapit@gmail.com>
2020-04-15 21:56:36 +0700

    ch

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name: 'Agent',
+    script: 'index.js',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      PORT: 3000,
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      PORT: 80,
+      NODE_ENV: 'production'
+    }
+  }],
+
+ 
+};
